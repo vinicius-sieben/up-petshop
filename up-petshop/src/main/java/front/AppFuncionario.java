7d7b8bb50d39deaa3e07d45625dcef7a5886b88f
mynameisfarelo <viniciussieben7@gmail.com>
2023-05-30 17:13:17 -0300

    Iniciado CRUD Funcionario

--- up-petshop/src/main/java/front/AppFuncionario.java
+++ up-petshop/src/main/java/front/AppFuncionario.java
@@ -2,6 +2,36 @@ package front;
 
 public class AppFuncionario {
 	public AppFuncionario() {
-		System.out.println("\nTeste funcionario");
+
+		int op;
+
+		do {
+			System.out.println("\n\n*** PAINEL DE FUNCIONARIO ***");
+			System.out.println("[1] - Cadastrar funcionario");
+			System.out.println("[2] - Listar funcionarios");
+			System.out.println("[3] - Consultar funcionario");
+			System.out.println("[4] - Atualizar funcionario");
+			System.out.println("[5] - Deletar funcionario");
+			System.out.println("[6] - Voltar");
+			op = Console.readInt("Selecione uma opcao: ");
+			switch (op) {
+			case 1:
+				// incluirFuncionario();
+				break;
+			case 2:
+				// listarFuncionarios();
+				break;
+			case 3:
+				// consultarFuncionario();
+				break;
+			case 4:
+				// atualizarFuncionario();
+				break;
+			case 5:
+				// deletarFuncionario();
+				break;
+			}
+
+		} while (op != 6);
 	}
 }
